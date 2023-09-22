595466ce246547c15cef595c31af7167ba4fe2bc
Holland Reece Brown <86682821+holland-reece@users.noreply.github.com>
2023-09-22 14:55:43 -0400

    init Connectome Workbench version

---
+++ evo_rest_lower_level_v3.py
@@ -0,0 +1,16 @@
+# EVO Lower-level ROI analysis - Connectome Workbench version
+
+# Holland Brown
+
+# Updated 2023-09-22
+# Created 2023-09-22
+
+#---------------------------------------------------------------------------------------------------------------
+
+# %%
+import tqdm
+from my_imaging_tools import fmri_tools
+
+datadir = f''
+
+q = fmri_tools(datadir)
