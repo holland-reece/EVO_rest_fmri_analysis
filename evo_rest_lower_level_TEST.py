9a9c79715642fbb5f90e9bcfa997ad822191b444
Holland Brown <hrreagor.brown@gmail.com>
2023-10-17 14:41:21 -0400

    fix incorrect dirs

--- evo_rest_lower_level_TEST.py
+++ evo_rest_lower_level_TEST.py
@@ -7,20 +7,27 @@ import tqdm
 # import nilearn as nil
 from my_imaging_tools import fmri_tools
 
-datadir = f'/home/holland/Desktop/EVO_TEST' # where subject folders are located
+datadir = f'/home/holland/Desktop/EVO_TEST/subjects' # where subject folders are located
 atlasdir = f'/home/holland/Desktop/EVO_atlas_search' # where HCP MMP1.0 files are located (downloaded from BALSA)
 rois = ['L_MFG'] # names of ROI text files containing HCP MMP1.0 ROI label names
 q = fmri_tools(datadir)
 
+# HCP MMP1.0 parcel labels (either *.dlabel.nii or *.dscalar.nii files)
+atlas_labels_left = f'/home/holland/Desktop/EVO_TEST/GlasserHumanCorticalParcellations_n8_6V6gD/Q1-Q6_RelatedValidation210.L.CorticalAreas_dil_Final_Final_Areas.32k_fs_LR.dscalar.nii'
+atlas_labels_right = f'/home/holland/Desktop/EVO_TEST/GlasserHumanCorticalParcellations_n8_6V6gD/Q1-Q6_RelatedValidation210.R.CorticalAreas_dil_Final_Final_Areas.32k_fs_LR.dscalar.nii'
+
 
 # %% Create ROI mask
-cmd = [None]*3
-for sub in tqdm(q.subs):
+cmd = [None]
+for sub in q.subs:
     for roi in rois:
-        subjectdir = f'{datadir}/{sub}'
+        subjectdir = f'{datadir}/subjects/{sub}'
         input_cifti = f'{subjectdir}'
         subject_left_surface = f'{subjectdir}'
         subject_right_surface = f'{subjectdir}'
         output_roi = f'{subjectdir}'
-        cmd[0] = f'wb_command -cifti-roi -cifti {input_cifti}.dscalar.nii -roi_file {roi}.txt -left_surface {subject_left_surface}.gii -right-surface {subject_right_surface}.gii -output {output_roi}.dscalar.nii'
+        # cmd[0] = f'wb_command -cifti-roi -cifti {input_cifti}.dscalar.nii -roi-file {roi}.txt -left_surface {subject_left_surface}.gii -right-surface {subject_right_surface}.gii -output {output_roi}.dscalar.nii'
+        cmd[0] = f'wb_command -cifti-label-to-roi {atlas_labels_left} {subjectdir}/{roi}.dscalar.nii -name L_IFSa_ROI'
         q.exec_cmds(cmd)
+
+# %%
