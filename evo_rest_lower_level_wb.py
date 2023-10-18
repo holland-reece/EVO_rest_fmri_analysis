abb42e38e014a59109debaf3b3676ca1569782f4
Holland Brown <hrreagor.brown@gmail.com>
2023-10-18 10:42:21 -0400

    added wholebrain corr & cleanup blocks

--- evo_rest_lower_level_wb.py
+++ evo_rest_lower_level_wb.py
@@ -41,73 +41,92 @@ x correlation_${sub}_average.dscalar.nii
 import os
 import tqdm
 import json
+import glob
 from my_imaging_tools import fmri_tools
 # from nipype import Node, Function
 # from nipype.interfaces.workbench.cifti import WBCommand
 
 datadir = f'/home/holland/Desktop/EVO_TEST/subjects' # where subject folders are located
-atlasdir = f'/home/holland/Desktop/EVO_atlas_search' # where HCP MMP1.0 files are located (downloaded from BALSA)
-rois = ['MFG'] # names of ROI text files containing HCP MMP1.0 ROI label names
-atlas_labels = f'' # HCP MMP1.0 parcel labels (either *.dlabel.nii or *.dscalar.nii files)
+atlasdir = f'/home/holland/Desktop/EVO_TEST/Glasser_et_al_2016_HCP_MMP1.0_kN_RVVG/HCP_PhaseTwo/Q1-Q6_RelatedValidation210/MNINonLinear/fsaverage_LR32k' # where HCP MMP1.0 files are located (downloaded from BALSA)
+roi = 'MFG' # names of ROI text files containing HCP MMP1.0 ROI label names
+parcels = ['L_IFSa_ROI','R_IFSa_ROI','L_46_ROI','R_46_ROI','L_p9-46v_ROI','R_p9-46v_ROI'] # names of HCP-MMP1.0 parcels for this ROI
+# parcels = ['R_IFSa_ROI','R_46_ROI','R_p9-46v_ROI']
+# parcels = ['IFSa', '46', 'p9-46v']
+atlas_labels = f'{atlasdir}/Q1-Q6_RelatedValidation210.CorticalAreas_dil_Final_Final_Areas_Group_Colors.32k_fs_LR.dlabel.nii' # HCP MMP1.0 parcel labels (either *.dlabel.nii or *.dscalar.nii files)
 
 q = fmri_tools(datadir)
 sessions = ['1','2']
 
 
-# %% FIX: give multiple ROIs as inputs to cifti-label-to-roi? see cmd[0] '-name' option
-# Create ROI mask from HCP-MMP1.0 atlas
+# %% Create ROI mask from HCP-MMP1.0 atlas
 cmd = [None]*3
 for sub in q.subs:
     for session in sessions:
-        subdir = f'{datadir}/{sub}/func/rest/session_{session}/run_1'
+        subdir = f'{datadir}/{sub}/func/rois/{roi}'
         if os.path.isdir(subdir):
-            for roi in rois:
-                input_cifti = f'{subdir}'
-                subject_left_surface = f'{subdir}'
-                subject_right_surface = f'{subdir}'
-                output_roi = f'{subdir}/{roi}_S{session}_R1'
+            input_cifti = f'{datadir}/{sub}/func/rest/session_{session}/run_1/Rest_ICAAROMA.nii.gz/denoised_func_data_aggr_s1.7.dtseries.nii'
+            output_roi = f'{subdir}/{roi}_S{session}_R1'
+            
+            # create subject-specific binary ROI mask from HCP-MMP1.0 parcels
+            command = [None]
+            for p in parcels:
+                command[0] = f'wb_command -cifti-label-to-roi {atlas_labels} {subdir}/{roi}_{p}_S{session}_R1.dscalar.nii -name {p}'
+                q.exec_cmds(command)
+
+            # concatenate parcels into our ROIs, and binarize
+            parc_files = glob.glob(f'{subdir}/*dscalar.nii')
+            print(len(parc_files))
+            roi_parcs = []
+            for pfile in parc_files:
+                if (f'{roi}' in pfile) and (f'S{session}' in pfile):
+                    roi_parcs.append(pfile)
+            print(roi_parcs)
+            cmd[0] = f"wb_command -cifti-math '(mask1 + mask2 + mask3 + mask4 + mask5 + mask6) > 0' {output_roi}_bin.dscalar.nii -var 'mask1' {roi_parcs[0]} -var 'mask2' {roi_parcs[1]} -var 'mask3' {roi_parcs[2]} -var 'mask4' {roi_parcs[3]} -var 'mask5' {roi_parcs[4]} -var 'mask6' {roi_parcs[5]}"
+            # print(cmd[0])
                 
-                # create subject-specific binary ROI mask from HCP-MMP1.0 parcels
-                cmd[0] = f'wb_command -cifti-label-to-roi {atlas_labels} {subdir}/{output_roi}.dscalar.nii -name IFSa_ROI'
+            # separate roi mask into left and right GIFTI surface hemispheres
+            cmd[1] = f'wb_command -cifti-separate {output_roi}_bin.dscalar.nii COLUMN -metric CORTEX_RIGHT {output_roi}_bin.R.shape.gii'
+            cmd[2] = f'wb_command -cifti-separate {output_roi}_bin.dscalar.nii COLUMN -metric CORTEX_LEFT {output_roi}_bin.L.shape.gii'
+            q.exec_cmds(cmd)
 
-                # separate roi mask into left and right GIFTI surface hemispheres
-                cmd[1] = f'wb_command -cifti-separate {output_roi}.dscalar.nii COLUMN -metric CORTEX_LEFT {output_roi}.L.shape.gii'
-                cmd[2] = f'wb_command -cifti-separate {output_roi}.dscalar.nii COLUMN -metric CORTEX_RIGHT {output_roi}.R.shape.gii'
-                q.exec_cmds(cmd)
+# %% Use binary ROI mask for roi-to-wholebrain analysis
+rois = ['MFG']
+hemispheres = ['L','R']
+cmd = [None]*4
+for sub in q.subs:
+    for session in sessions:
+        for hemisphere in hemispheres:
+            func_in = f'{datadir}/{sub}/func/rest/session_{session}/run_1/Rest_ICAAROMA.nii.gz/denoised_func_data_aggr_s1.7.dtseries.nii'
+            roidir = f'{datadir}/{sub}/func/rois/{roi}'
+            roi_in = f'{roidir}/{roi}_S{session}_R1_bin.{hemisphere}.shape.gii'
+            
+            # take average of func time series in the ROI
+            # cmd[0] = f'wb_command -cifti-roi-average ${subject}_${run}.dtseries.nii roi_data_${sub}_${run}_unsmoothed.txt -vol-roi ${sub}_roi_mask.nii'
+            if hemisphere == 'L':
+                h = 'left'
+            elif hemisphere == 'R':
+                h = 'right'
+            cmd[0] = f'wb_command -cifti-roi-average {func_in} {roidir}/{hemisphere}_{roi}_S{session}_R1_mean.txt -{h}-roi {roi_in}'
 
+            # convert roi text file to dscalar
+            cmd[1] = f'wb_command -cifti-create-scalar-series {roidir}/{hemisphere}_{roi}_S{session}_R1_mean.txt {roidir}/{hemisphere}_{roi}_S{session}_R1_mean.dscalar.nii -transpose -series SECOND 0 1'
 
-# %% (1) Extract ROI timeseries from ciftis, convert to text files, convert to CIFTIS
+            # get correlation of average ROI time series with time series of every voxel in the brain
+            cmd[2] = f'wb_command -cifti-correlation {func_in} {roidir}/{hemisphere}_{roi}_S{session}_R1_wholebrain_corr.dconn.nii -cifti-roi {roidir}/{hemisphere}_{roi}_S{session}_R1_mean.dscalar.nii'
 
-# cmd_list = [None]*3 # allocate memory for commands
-# for sub in tqdm(q.subs):
-#     for roi in rois:
-#         for session in sessions:
-#             # nifti_input = f'{datadir}/{sub}/{sub}_{preproc_folder}/{sub}_{nifti_fn}' # subject's preprocessed AFNI ICA results dir
-#             nifti_input = f''
-#             new_roi_path = f'' # subject's roi analysis dir
-#             tsAlready = os.path.exists(f'{new_roi_path}') # check if time series file already exists for this participant
-#             if tsAlready == False: # if time series file doesn't exist...
-#                 print(f'Extracting ROI time series for {sub}...\n')
+            # convert to z-scores
+            cmd[3] = f'wb_command -cifti-math "atanh(r)" {roidir}/{hemisphere}_{roi}_S{session}_R1_wholebrain_corr_zscore.dconn.nii -var "r" {roidir}/{hemisphere}_{roi}_S{session}_R1_wholebrain_corr.dconn.nii'
 
-#                 # NEED ROI MASK NIFTI INPUT; also identify input cifti
-#                 roi_label = f'{datadir}/{sub}/anat/T1w/{sub}/label/{label_gifti}'
-#                 cmd_list[0] = 'wb_command -gifti-label-to-roi' # identify an ROI from anatomical parc labels and extract roi cifti
-#                 # cmd_list[1] = f'wb_command -cifti-create-dense-from-template -label {roi_label}' # use label files generated by MEP
-#                 cmd_list[1] = f'wb_command -cifti-roi-average {sub}_{roi}_S{session}.dtseries.nii {sub}_{roi}_S{session}.txt -vol-roi {sub}_roi_mask.nii.gz'
-#                 cmd_list[2] = f'wb_command -cifti-create-scalar-series roi_data_${sub}_${run}_unsmoothed.txt roi_data_${sub}_${run}_unsmoothed.dscalar.nii -transpose -series SECOND 0 1'
-#                 q.exec_cmds(cmd_list) # execute bash commands in system terminal
-#             else:
-#                 print(f'{roi} time series file already exists for subject {sub}...\n')
+            # filter by significance of p < 0.05
+            # cmd[2] = f'wb_command -metric-math "(p < 0.05)" {roidir}/{hemisphere}_{roi}_S{session}_R1_wholebrain_corr_zscore_significant.func.gii -var "p" statistical_map.func.gii'
 
+            q.exec_cmds(cmd)
+            
 
 # %% Cross-correlate this new ROI timeseries cifti with the smoothed whole-brain cifti data
 # wb_command -cifti-cross-correlation ${subject}_${run}_smoothed.dtseries.nii 
 # roi_data_${sub}_${run}_unsmoothed.dscalar.nii 
 # correlation_${sub}_${run}.dscalar.nii
-
-
-
-
 
 
 # %% ROI-to-wholebrain correlation
@@ -148,4 +167,19 @@ wb_command -cifti-average-roi-correlation
 
 # wb_command -cifti-math "atanh(x)" z_correlation_${sub}_average.dscalar.nii -var 
 # x correlation_${sub}_average.dscalar.nii
-  
+
+# %% Cleaning up
+rois = ['MFG']
+cmd = [None]
+for sub in q.subs:
+    parc_dir = q.create_dirs(f'{datadir}/{sub}/func/rois/{roi}/{roi}_parcels')
+    for roi in rois:
+        for p in parcels:
+            roi_files = glob.glob(f'{datadir}/{sub}/func/rois/{roi}/*.dscalar.nii')
+            for file in roi_files:
+                if p in file:
+                    cmd[0] = f'mv {file} {parc_dir}'
+                    # q.exec_cmds(cmd)
+
+
+# %%
