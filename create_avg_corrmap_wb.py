b97fcbc145d0d469a031697afdcdbc6b57abea66
Holland Reece Brown <86682821+holland-reece@users.noreply.github.com>
2023-11-03 12:36:08 -0400

    add opt for excluding outliers

--- create_avg_corrmap_wb.py
+++ create_avg_corrmap_wb.py
@@ -13,11 +13,27 @@ import os
 import glob
 from my_imaging_tools import fmri_tools
 
-datadir = f'/athena/victorialab/scratch/hob4003/study_EVO/UW_MRI_data' # where subject folders are located
+site = 'NKI'
+datadir = f'/athena/victorialab/scratch/hob4003/study_EVO/{site}_MRI_data' # where subject folders are located
 scriptdir = f'/athena/victorialab/scratch/hob4003/study_EVO/EVO_rs_lower_levels' # where this script, atlas, and my_imaging_tools script are located
-atlasdir = f'{scriptdir}/Glasser_et_al_2016_HCP_MMP1.0_kN_RVVG/HCP_PhaseTwo/Q1-Q6_RelatedValidation210/MNINonLinear/fsaverage_LR32k' # where HCP MMP1.0 files are located (downloaded from BALSA)
-atlas_labels = f'{atlasdir}/Q1-Q6_RelatedValidation210.CorticalAreas_dil_Final_Final_Areas_Group_Colors.32k_fs_LR.dlabel.nii' # HCP MMP1.0 parcel labels (either *.dlabel.nii or *.dscalar.nii files)
 wb_command = f'/software/apps/Connectome_Workbench_test/workbench/exe_rh_linux64/wb_command' # /path/to/wb_command package
 
 q = fmri_tools(datadir)
 sessions = ['1','2']
+rois=['L_MFG','R_MFG','L_dACC','R_dACC','L_rACC','R_rACC']
+#subs = q.read_temp_sublist_txt('/athena/victorialab/scratch/hob4003/study_EVO/NKI_subjectlist.txt')
+
+# %%
+cmd=[None]
+for session in sessions:
+    for roi in rois:
+        cifti_list_str = ''
+        cifti_out = f'{scriptdir}/EVO_lower_level_avg_corrmaps/{roi}_S{session}_lowerlev_{site}avg_corrmap.dscalar.nii'
+        corr_maps = glob.glob(f'{datadir}/*/func/rois/{roi}/*_R1_denoised_aggr_s1.7_wholebrain_crosscorrmap.dscalar.nii')
+        # exclude_outliers_opt=f'-exclude-outliers <stddevs-below> <stddevs-above>'
+        exclude_outliers_opt=f'' # don't exclude outliers from avg corrmap
+        print(corr_maps)
+        for map in corr_maps:
+            cifti_list_str = f'{cifti_list_str} -cifti {map}'
+        cmd[0] = f'{wb_command} -cifti-average {cifti_out} {exclude_outliers_opt}{cifti_list_str}'
+        q.exec_cmds(cmd)
