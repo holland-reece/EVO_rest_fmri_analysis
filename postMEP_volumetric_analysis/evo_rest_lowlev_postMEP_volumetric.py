24f7cca06e7fdeee7fae378e974a6fa6f1072609
Holland Reece Brown <86682821+holland-reece@users.noreply.github.com>
2023-12-22 14:01:57 -0500

    also run analysis version with binarize threshold 0.8

--- postMEP_volumetric_analysis/evo_rest_lowlev_postMEP_volumetric.py
+++ postMEP_volumetric_analysis/evo_rest_lowlev_postMEP_volumetric.py
@@ -2,7 +2,7 @@
 
 # Holland Brown
 
-# Updated 2023-12-21
+# Updated 2023-12-22
 # Created 2023-11-28
 
 # Separate linear model for each subject, 2 repeated measures (sessions), 6 ROIs
@@ -22,25 +22,21 @@ import glob
 from my_imaging_tools import fmri_tools
 
 site = 'NKI'
-# datadir = f'/athena/victorialab/scratch/hob4003/study_EVO' # where subject folders are located
-# scriptdir = f'/athena/victorialab/scratch/hob4003/study_EVO/EVO_rs_lower_levels' # where this script, atlas, and my_imaging_tools script are located
-# wb_command = f'/software/apps/Connectome_Workbench_test/workbench/exe_rh_linux64/wb_command' # /path/to/wb_command package
+home_dir = f'/athena/victorialab/scratch/hob4003/study_EVO/EVO_rest/EVO_rest_volumetric'
+datadir = f'/athena/victorialab/scratch/hob4003/study_EVO/{site}_MRI_data' # where subject folders are located
+# identity_mat = f'/athena/victorialab/scratch/hob4003/ME_Pipeline/MEF-P-HB/MultiEchofMRI-Pipeline/res0urces/ident.mat'
 
-home_dir = f'/home/holland/Desktop/EVO_TEST' # where subject folders are located
-datadir = f'{home_dir}/subjects' # where this script, atlas, and my_imaging_tools script are located
-identity_mat = f'/home/holland/Documents/GitHub_repos/ME-fMRI-Pipeline-double-echo-fieldmaps/res0urces/ident.mat'
-
-# fsl_template = f"/home/holland/fsl/data/standard/MNI152_T1_2mm_brain"
-# wb_command = f'wb_command' # /path/to/wb_command package, or just 'wb_command'
+# home_dir = f'/home/holland/Desktop/EVO_TEST' # where subject folders are located
+# datadir = f'{home_dir}/subjects' # where this script, atlas, and my_imaging_tools script are located
+# identity_mat = f'/home/holland/Documents/GitHub_repos/ME-fMRI-Pipeline-double-echo-fieldmaps/res0urces/ident.mat'
 
 q = fmri_tools(datadir)
-sessions = ['2']
+sessions = ['1','2']
 runs = ['1']
 rois=['L_MFG','R_MFG','L_dACC','R_dACC','L_rACC','R_rACC']
-# func_fn = 'denoised_func_data_aggr' # without extension; ac/pc aligned, denoised with ICA-AROMA
-func_fn = 'denoised_func_data_aggr'
+func_fn = 'denoised_func_data_aggr' # denoised volumetric func data
 
-# %% First, align HCP-MMP1 in subject's FreeSurfer space to subject's anatomical -> get right pixel dims
+# %% First, align HCP-MMP1 in subject's FreeSurfer space to subject's anatomical -> get transform matrix
 cmd=[None]*2
 for sub in q.subs:
     for session in sessions:
@@ -49,20 +45,15 @@ for sub in q.subs:
             glasser_atlas_in = f'{sub_parc_niftis_dir}/HCP-MMP1' # input subject atlas filename (no ext)
             glasser_atlas_out = f'{sub_parc_niftis_dir}/HCP-MMP1_denoiseaggrfunc_S{session}_R{run}' # output subject atlas filename (no ext)
             # flirt_reference = f'{datadir}/{sub}/func/rest/session_{session}/run_{run}/Rest_ICAAROMA.nii.gz/{func_fn}' # reference for Flirt ailgnment: functional data
-            flirt_reference = f'{datadir}/{sub}/func/xfms/T1w_acpc_brain_func'
+            flirt_reference = f'{datadir}/{sub}/func/xfms/rest/T1w_acpc_brain_func'
 
-
-            cmd[0] = f'fslreorient2std {glasser_atlas_in} {glasser_atlas_in}_reoriented' # reorient FS HCP-MMP1 to FSL standard orientation
-            cmd[1] = f"flirt -interp nearestneighbour -in {glasser_atlas_in}_reoriented.nii.gz -ref {flirt_reference}.nii.gz -out {glasser_atlas_out}.nii.gz -omat {glasser_atlas_out}.mat" # flirt alignment to func space -> get transform matrix
-            # cmd[2] = f'flirt -interp nearestneighbor -in {func_in} {glasser_atlas_in}_reoriented.nii.gz' # register func data to subject FS space
-            q.exec_cmds(cmd)
-
+            if (os.path.isfile(glasser_atlas_out)==False) and (os.path.isdir(sub_parc_niftis_dir)==True):
+                cmd[0] = f'fslreorient2std {glasser_atlas_in} {glasser_atlas_in}_reoriented' # reorient FS HCP-MMP1 to FSL standard orientation
+                cmd[1] = f"flirt -interp nearestneighbour -in {glasser_atlas_in}_reoriented.nii.gz -ref {flirt_reference}.nii.gz -out {glasser_atlas_out}.nii.gz -omat {glasser_atlas_out}.mat" # flirt alignment to func space -> get transform matrix
+                q.exec_cmds_prtsubject(cmd,f'{sub}_S{session}_R{run}')
 
 # %% Second, concat ROI parcels into subject-specific ROI masks and align them to subjects' functional space; then, extract ROI time series
-# rois=['R_MFG','L_dACC','R_dACC','L_rACC','R_rACC']
-# rois = ['L_MFG']
-
-cmd = [None]*6
+cmd = [None]*8
 for roi in rois:
 
     # ROI parcel names from HCP MMP1.0 atlas labels
@@ -80,42 +71,48 @@ for roi in rois:
         roi_parcels = ['L_p24_ROI','L_a24_ROI'] # L_rACC
 
     for sub in q.subs:
+        sub_parc_niftis_dir = f'{datadir}/{sub}/anat/{sub}_HCP-MMP1_vol_roi_masks' # path to Glasser atlas in subject func space
         for session in sessions:
             for run in runs:
-                # directories
-                roidir = f'{datadir}/{sub}/func/rest/rois/{roi}/{sub}_native_space_volumetric' # output dir for volumetric roi analysis
-                sub_parc_niftis_dir = f'{datadir}/{sub}/anat/{sub}_HCP-MMP1_vol_roi_masks' # subject's HCP-MMP1 roi masks dir
+                glasser_atlas_out = f'{sub_parc_niftis_dir}/HCP-MMP1_denoiseaggrfunc_S{session}_R{run}.nii.gz'
+                if os.path.isfile(f'{glasser_atlas_out}'):
+                    # directories
+                    roidir = f'{datadir}/{sub}/func/rest/rois/{roi}/rest_lowerlev_vol' # output dir for volumetric roi analysis
+                    sub_parc_niftis_dir = f'{datadir}/{sub}/anat/{sub}_HCP-MMP1_vol_roi_masks' # subject's HCP-MMP1 roi masks dir
 
-                # files for mask creation & Flirt alignment
-                # flirt_reference = f'{datadir}/{sub}/func/rest/session_{session}/run_{run}/{func_fn}'
-                # flirt_reference = f'{sub_parc_niftis_dir}/HCP-MMP1_denoiseaggrfunc_S{session}_R{run}' # ref img for ROI mask Flirt realign: funcspace subject Glasser atlas
-                flirt_reference = f'{datadir}/{sub}/func/xfms/T1w_acpc_brain_func'
-                mask_out = f'{roidir}/{roi}_S{session}_R{run}' # mask comprised of Glasser ROI parcels; prefix for other ROI mask output files
+                    # files for mask creation & Flirt alignment
+                    flirt_reference = f'{datadir}/{sub}/func/xfms/rest/T1w_acpc_brain_func'
+                    mask_out = f'{roidir}/{roi}_S{session}_R{run}' # mask comprised of Glasser ROI parcels; prefix for other ROI mask output files
 
-                # files for time-series extraction
-                func_in = f'{datadir}/{sub}/func/rest/session_{session}/run_{run}/{func_fn}'
-                roi_ts = f'{roidir}/{roi}_S{session}_R{run}_timeseries'
+                    # files for time-series extraction
+                    func_in = f'{datadir}/{sub}/func/rest/session_{session}/run_{run}/Rest_ICAAROMA.nii.gz/{func_fn}'
+                    roi_ts = f'{roidir}/{roi}_S{session}_R{run}_timeseries'
 
-                # Create subject volumetric ROI dir if needed
-                if os.path.isdir(roidir)==False:
-                    q.create_dirs(roidir)
+                    # Create subject volumetric ROI dir if needed
+                    if os.path.isdir(roidir)==False:
+                        q.create_dirs(roidir)
 
-                # Structure fslmaths command string with all roi parcels
-                cmd_str = f'fslmaths'
-                for p in roi_parcels:
-                    if p == roi_parcels[0]:
-                        cmd_str = f'{cmd_str} {sub_parc_niftis_dir}/masks/{p}'
-                    else:
-                        cmd_str = f'{cmd_str} -add {sub_parc_niftis_dir}/masks/{p}'
+                    # Structure fslmaths command string with all roi parcels
+                    cmd_str = f'fslmaths'
+                    for p in roi_parcels:
+                        if p == roi_parcels[0]:
+                            cmd_str = f'{cmd_str} {sub_parc_niftis_dir}/masks/{p}'
+                        else:
+                            cmd_str = f'{cmd_str} -add {sub_parc_niftis_dir}/masks/{p}'
 
-                cmd[0] = f'{cmd_str} {mask_out}' # combine Glasser ROI parcels into roi mask with fslmaths
-                cmd[1] = f'fslreorient2std {mask_out} {mask_out}_reoriented' # reorient HCP-MMP1 masks to FSL standard orientation
-                cmd[2] = f'flirt -2D -in {mask_out}_reoriented.nii.gz -ref {flirt_reference}.nii.gz -out {mask_out}_denoiseaggrfunc.nii.gz -omat {mask_out}_denoiseaggrfunc.mat' # 2D align ROI mask with func
-                cmd[3] = f'fslmaths {func_in}.nii.gz -add 10000 {func_in}_remean.nii.gz' # recenter ROI mask at 10000
-                cmd[4] = f'fslmaths {mask_out}_denoiseaggrfunc.nii.gz -thr 0.3 -bin {mask_out}_denoiseaggrfunc_bin_thr0.3.nii.gz' # binarize
-                cmd[5] = f'fslmeants -i {func_in}_remean.nii.gz -o {roi_ts}_thr0.3.txt -m {mask_out}_denoiseaggrfunc_bin_thr0.3.ni.gz' # calculate mean time series; function takes (1) path to input NIfTI, (2) path to output text file, (3) path to mask NIfTI
-                q.exec_cmds(cmd)
-  
+                    q.exec_echo(f'{sub}_S{session}_{run}\t{roi}')
+
+                    cmd[0] = f'{cmd_str} {mask_out}' # combine Glasser ROI parcels into roi mask with fslmaths
+                    cmd[1] = f'fslreorient2std {mask_out} {mask_out}_reoriented' # reorient HCP-MMP1 masks to FSL standard orientation
+                    cmd[2] = f'flirt -2D -in {mask_out}_reoriented.nii.gz -ref {flirt_reference}.nii.gz -out {mask_out}_denoiseaggrfunc.nii.gz -omat {mask_out}_denoiseaggrfunc.mat' # 2D align ROI mask with func
+                    cmd[3] = f'fslmaths {func_in}.nii.gz -add 10000 {func_in}_remean.nii.gz' # recenter ROI mask at 10000
+                    cmd[4] = f'fslmaths {mask_out}_denoiseaggrfunc.nii.gz -bin {mask_out}_denoiseaggrfunc_bin.nii.gz' # binarize
+                    cmd[5] = f'fslmaths {mask_out}_denoiseaggrfunc.nii.gz -bin -thr 0.8 {mask_out}_denoiseaggrfunc_bin0.8.nii.gz' # binarize with threshold 0.8
+                    cmd[6] = f'fslmeants -i {func_in}_remean.nii.gz -o {roi_ts}.txt -m {mask_out}_denoiseaggrfunc_bin.nii.gz' # calculate mean time series; function takes (1) path to input NIfTI, (2) path to output text file, (3) path to mask NIfTI
+                    cmd[7] = f'fslmeants -i {func_in}_remean.nii.gz -o {roi_ts}_thr0.8.txt -m {mask_out}_denoiseaggrfunc_bin0.8.nii.gz' # calculate mean time series from mask with threshold 0.8
+                    q.exec_cmds(cmd)
+q.exec_echo('\nDone.')
+
 # %% 6. Run lower-level analysis using design template (ref: first_level5.sh)
 feat_fn = f'20231221_evo_lowerlev_test2.fsf'
 feat_df = f'/home/holland/Desktop/{feat_fn}'
