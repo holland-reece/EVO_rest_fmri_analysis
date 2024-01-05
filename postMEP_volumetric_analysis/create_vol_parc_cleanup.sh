84964b5ae32a8f8821e21eedd6e1d3667fcf06e5
Holland Reece Brown <86682821+holland-reece@users.noreply.github.com>
2024-01-05 09:33:14 -0500

    debug: mismatched 'if' and 'fi'

--- postMEP_volumetric_analysis/create_vol_parc_cleanup.sh
+++ postMEP_volumetric_analysis/create_vol_parc_cleanup.sh
@@ -1,20 +1,27 @@
 #!/bin/bash
 # Holland Brown
-# 2023-12-13
+# 2023-01-05
 # Fix mask dir tree and copy ROI masks to main subject dirs
 
 DataDir="/athena/victorialab/scratch/hob4003/study_EVO/NKI_MRI_data"
 MasksDir="/athena/victorialab/scratch/hob4003/study_EVO/EVO_rest/EVO_rest_volumetric/HCP-MMP1_fsaverage_masks"
-SubjectsList="/athena/victorialab/scratch/hob4003/study_EVO/NKI_MRI_data/NKIsublist.txt"
+SubjectsList="/athena/victorialab/scratch/hob4003/study_EVO/NKI_MRI_data/NKIsublist2.txt"
 
 for i in $(cat "$SubjectsList"); do
 
     SUBJ=$(echo $i)
+    echo -e "Cleaning $SUBJ files..."
 
-    mv "$MasksDir/$SUBJ" "$MasksDir"/"$SUBJ"_HCP-MMP1_vol_roi_masks # rename the mask folders
+    if [ -f "$DataDir/$SUBJ.txt" ]; then
+        rm "$DataDir/$SUBJ.txt"
+    fi
+
+    if [ ! -d "$MasksDir/$SUBJ_HCP-MMP1_vol_roi_masks" ]; then
+        mv "$MasksDir/$SUBJ" "$MasksDir/$SUBJ_HCP-MMP1_vol_roi_masks" # rename the mask folders
+    fi
 
     SubjectAnatDir="$DataDir"/"$SUBJ"/anat
-    cp -rf "$MasksDir"/"$SUBJ"_HCP-MMP1_roi_vol_masks "$DataDir"/"$SUBJ"/anat # make copy of ROI masks in subject's anat dir
+    cp -rf "$MasksDir"/"$SUBJ"_HCP-MMP1_vol_roi_masks "$DataDir"/"$SUBJ"/anat # make copy of ROI masks in subject's anat dir
 
     rm -r "$DataDir"/"$SUBJ"/anat/T1w/$SUBJ/symlinks # remove symbolic links from anat dir
 
@@ -30,3 +37,5 @@ for i in $(cat "$SubjectsList"); do
     rm -r "$DataDir/$SUBJ/trash"
 
 done
+
+echo -e "Done."
