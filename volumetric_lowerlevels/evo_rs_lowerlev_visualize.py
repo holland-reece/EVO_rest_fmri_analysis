0bb415fe306f024fa81f016630486d3862c8eae7
Holland Reece Brown <86682821+holland-reece@users.noreply.github.com>
2024-03-15 11:22:23 -0400

    debug: can't plot time series 1D array as carpet; use nifti

--- volumetric_lowerlevels/evo_rs_lowerlev_visualize.py
+++ volumetric_lowerlevels/evo_rs_lowerlev_visualize.py
@@ -5,16 +5,19 @@
 # Updated 2023-03-15
 # Created 2024-03-15
 
+# NOTE: numpy can't read NIFTIs; use nibabel to read in ROI masks for carpet plots
+
 # --------------------------------------------------------------------------------------
 # %%
 import os
 import subprocess
 import numpy as np
+# import glob
 import matplotlib.pyplot as plt
 from my_imaging_tools import fmri_tools
 
 # Important dirs
-home_dir = f'/media/holland/EVO_Estia/EVO_MRI/organized' # path to MNI brain template for FSL, fsf file, etc
+home_dir = f'/Volumes/EVO_Estia/EVO_MRI/organized' # path to MNI brain template for FSL, fsf file, etc
 
 # q = fmri_tools(studydir=datadir, subjectlist_text=args.subjecttextlist) # init functions and subject list
 
@@ -26,24 +29,22 @@ sites = ['NKI','UW'] # collection sites (also names of dirs)
 
 # %% Functions
 
+# Read a single-column text file containing ROI activation time series data
 def read_activation_file(filepath):
-    """
-    Reads a single-column text file containing ROI activation time series data.
-    """
     return np.loadtxt(filepath)
 
 def plot_carpet_plot(data, title, ax):
     """
-    Plots a carpet plot of ROI activation time series data.
+    Plot a carpet plot of ROI activation time series data!
     """
     ax.imshow(data, aspect='auto', cmap='jet')
     ax.set_title(title)
     ax.set_ylabel('ROI')
     ax.set_xlabel('Time')
 
-def process_directory(directory_path):
+def plot_activations(directory_path, figfilename):
     """
-    Processes a single directory, plotting carpet plots for each file and returning the average activations.
+    Process a single directory, plotting carpet plots for each file and returning the average activations
     """
     activations = []
     fig, axs = plt.subplots(nrows=len(os.listdir(directory_path)), ncols=1, figsize=(10, 3 * len(os.listdir(directory_path))))
@@ -57,21 +58,25 @@ def process_directory(directory_path):
             activation_data = read_activation_file(filepath)
             activations.append(activation_data)
             plot_carpet_plot(activation_data, filename, axs[i])
+
+            # figpath = os.path.join(directory_path, f'{figfilename}.png')
+            plt.savefig(figfilename)
+            plt.close()
     
-    plt.tight_layout()
-    plt.show()
+    # plt.tight_layout()
+    # plt.show()
     
     return np.mean(activations, axis=0)
 
-def plot_average_activations(directories):
+def plot_average_activations(directories, fig_fullpath):
     """
-    Plots the average ROI activations from each directory in a single plot.
+    Plot the average ROI activations from each directory in a single plot
     """
     average_activations = []
     labels = []
     
     for directory_path in directories:
-        avg_activation = process_directory(directory_path)
+        avg_activation = plot_activations(directory_path, fig_fullpath)
         average_activations.append(avg_activation)
         labels.append(os.path.basename(directory_path))
     
@@ -83,6 +88,38 @@ def plot_average_activations(directories):
     plt.xlabel('Time')
     plt.ylabel('Activation')
     plt.legend()
-    plt.show()
+    # plt.show()
+    plt.savefig(f'{fig_fullpath}.png')
+    plt.close()
 
+# %% For each subject, plot carpet plots
+figure_path = f'{home_dir}/EVO_rest_lower_levels'
+
+dirlist = []
+for site in sites:
+    datadir = f'{home_dir}/{site}'
+    q = fmri_tools(datadir)
+    for sub in q.subs:
+        for roi in rois:
+            for session in sessions:
+                for run in runs:
+                    featdir = f'{datadir}/{sub}/func/rest/rois/{roi}/rest_lowerlev_vol/S{session}_R{run}_lowerlev_vol.feat'
+                    plot_fn = f'{datadir}/{sub}/func/rest/rois/{roi}/rest_lowerlev_vol/'
+                    if os.path.isdir(featdir)==False:
+                        print(f'{sub} has no {roi} Feat directory.')
+                    else:
+                        dirlist.append(f'{featdir}')
+
+                plot_average_activations(dirlist, f'{home_dir}/EVO_rest_lower_levels_{roi}_S{session}.png')
+
+
+
+                        
+
+
+# %% Test
+subjectdir = f'/Volumes/EVO_Estia/EVO_MRI/organized/NKI/97048/func/rest/rois/L_MFG/rest_lowerlev_vol/L_MFG_S1_R1_denoiseaggrfunc_bin0.8.nii.gz'
+# plot_activations(subjectdir, f'/Volumes/EVO_Estia/EVO_MRI/organized/NKI/97048/func/rest/rois/L_MFG/rest_lowerlev_vol/L_MFG_ts_plot.png')
+roidata = read_activation_file(subjectdir)
+print(roidata)
 # %%
