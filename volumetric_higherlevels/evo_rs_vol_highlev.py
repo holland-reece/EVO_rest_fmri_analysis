306e2c1099ee788b9375aa1cb9b04bd6f9767da1
Holland Reece Brown <86682821+holland-reece@users.noreply.github.com>
2024-04-12 14:25:25 -0400

    add mixed effects linear model and plots

--- volumetric_higherlevels/evo_rs_vol_highlev.py
+++ volumetric_higherlevels/evo_rs_vol_highlev.py
@@ -20,39 +20,57 @@ from my_imaging_tools import fmri_tools
 import csv
 
 
-def create_mixed_effects_model(connectivity_data_session1, connectivity_data_session2, treatment_labels):
-    # Convert inputs to numpy arrays if they are not already
-    connectivity_data_session1 = np.array(connectivity_data_session1)
-    connectivity_data_session2 = np.array(connectivity_data_session2)
+# def create_mixed_effects_model(connectivity_data_session1, connectivity_data_session2, treatment_labels):
+#     # Convert inputs to numpy arrays if they are not already
+#     connectivity_data_session1 = np.array(connectivity_data_session1)
+#     connectivity_data_session2 = np.array(connectivity_data_session2)
 
-    # Create a DataFrame for the model
-    df = pd.DataFrame({
-        'Session1_Connectivity': connectivity_data_session1.flatten(),
-        'Session2_Connectivity': connectivity_data_session2.flatten(),
-        'Treatment': treatment_labels * connectivity_data_session1.shape[0]  # Repeat each treatment label for each subject
-    })
+#     # Create a DataFrame for the model
+#     df = pd.DataFrame({
+#         'Session1_Connectivity': connectivity_data_session1.flatten(),
+#         'Session2_Connectivity': connectivity_data_session2.flatten(),
+#         'Treatment': treatment_labels * connectivity_data_session1.shape[0]  # Repeat each treatment label for each subject
+#     })
 
-    # Fit the mixed effects model
-    formula = 'Session2_Connectivity ~ Session1_Connectivity + Treatment'
-    model = smf.mixedlm(formula, df, groups=df.index)
-    result = model.fit()
+#     # Fit the mixed effects model
+#     formula = 'Session2_Connectivity ~ Session1_Connectivity + Treatment'
+#     model = smf.mixedlm(formula, df, groups=df.index)
+#     result = model.fit()
 
-    return result
+    # return result
 
+def matrix_to_df(matrix, group, time):
+    df = pd.DataFrame(matrix)
+    df = df.stack().reset_index()
+    df.columns = ['Measurement', 'Subject', 'Value']
+    df['Time'] = time
+    df['Group'] = group
+    return df
 
+# Flatten the matrices and create a DataFrame
+# def flatten_data(matrix, group, time):
+#     # Flatten the matrix and turn it into a DataFrame
+#     subjects, x, y = matrix.shape
+#     df = pd.DataFrame(matrix.reshape(subjects, x*y).T, 
+#                       columns=[f'subject_{i+1}' for i in range(subjects)])
+#     df['Time'] = time
+#     df['Group'] = group
+#     df = df.melt(id_vars=['Time', 'Group'], var_name='Subject', value_name='Value')
+#     return df
 
 
 # Set up
-home_dir = f'/media/holland/EVO_Estia/EVO_MRI/organized' # path to MNI brain template for FSL, fsf file, etc
+# home_dir = f'/media/holland/EVO_Estia/EVO_MRI/organized' # path to MNI brain template for FSL, fsf file, etc
+home_dir = f'/Volumes/EVO_Estia/EVO_MRI/organized' # path to MNI brain template for FSL, fsf file, etc
 
 # q = fmri_tools(studydir=datadir, subjectlist_text=args.subjecttextlist) # init functions and subject list
 
 sessions = ['1','2']
 runs = ['1']
-# rois = ['L_MFG','R_MFG','L_dACC','R_dACC','L_rACC','R_rACC']
+# rois = ['R_MFG','L_dACC','R_dACC','L_rACC','R_rACC']
 rois = ['L_MFG'] # test
 sites = ['NKI','UW'] # collection sites (also names of dirs)
-num_subjects = 55
+num_subjects = 51 # number of subjects we want to include in higher levels
 
 
 
@@ -70,7 +88,7 @@ num_subjects = 55
 #             q.exec_cmds(cmd)
 
 # %% TEST: check number of volumes for all participants
-dir = f'{home_dir}/UW'
+dir = f'{home_dir}/NKI'
 q = fmri_tools(dir)
 cmd = [None]*2
 for sub in q.subs:
@@ -80,7 +98,7 @@ for sub in q.subs:
 
 
 # %% Read in Tx group list CSV file
-with open('/home/holland/Desktop/EVO_Tx_groups.csv', mode ='r')as file:
+with open('/Volumes/EVO_Estia/EVO_rest_higherlev_vol/EVO_Tx_groups.csv', mode ='r')as file:
     TxGroups = csv.reader(file)
     group_labels = []
     for line in TxGroups:
@@ -89,12 +107,13 @@ with open('/home/holland/Desktop/EVO_Tx_groups.csv', mode ='r')as file:
 
 # %% Use lower level COPE files as columns of connectivity matrices for each session and ROI
 TxLabels = []
-matrix = np.zeros((902629,55)) # for each subject, will reshape NIFTI into a vector -> 1 column
+# matrix = np.zeros((902629,55)) # for each subject, will reshape NIFTI into a vector -> 1 column
 # matrix = np.zeros(91,109,91,num_subjects)
 
 for roi in rois:
     for session in sessions:
-        matrix = np.zeros((902629,51)) # number of voxels by number of participants
+        matrix = np.zeros((902629,num_subjects)) # number of voxels by number of participants
+        # sum_mat = np.zeros((91,109,91)) # will use this for means later
         i = 0 # init counter (for indexing final output matrix)
         for site in sites:
             datadir = f'{home_dir}/{site}' # where subject dirs are located
@@ -104,8 +123,8 @@ for roi in rois:
                 # label_pair is a pair containing subject ID and treatment group
                 sub = label_pair[0]
                 Tx = label_pair[1]
-                # cope_path = f'{datadir}/{sub}/func/rest/rois/{roi}/rest_lowerlev_vol/S{session}_R1_lowerlev_vol.feat/rendered_thresh_zstat1.nii.gz'
-                cope_path = f'{datadir}/{sub}/func/rest/rois/{roi}/rest_lowerlev_vol/S{session}_R1_lowerlev_vol.feat/stats/cope1.nii.gz'
+                cope_path = f'{datadir}/{sub}/func/rest/rois/{roi}/rest_lowerlev_vol/S{session}_R1_lowerlev_vol.feat/stats/zstat1.nii.gz'
+                # cope_path = f'{datadir}/{sub}/func/rest/rois/{roi}/rest_lowerlev_vol/S{session}_R1_lowerlev_vol.feat/stats/cope1.nii.gz'
 
 
                 # check that FSL Feat stats dir exists
@@ -115,29 +134,35 @@ for roi in rois:
 
                     # load and flatten COPE data into a vector
                     nii = nib.load(cope_path) # read in NIFTI file
-                    data = nii.get_fdata() # reshape into vector (length: total number of voxels)
-                    
-                    # normalize COPE distribution so its magnitude is 1 (divide by l2 norm) -> puts all vectors on same scale
-                    # l2_norm = np.linalg.norm(data)
-                    # data_norm = data / l2_norm
-                    # if np.sum(data_norm) == 0: # check that vector is not all zeros
-                    #     print(f'{sub}, {roi}, S{session} has 0 sum...')
+                    data = nii.get_fdata() # get subject's lower-level result as numpy array
+                    print(f'{sub} {session} {np.shape(data)}')
+                    # sum_mat += data # add subject's lower-level result to sum
 
-                    # data_norm.tofile(norm_fn, sep=',') # save normalized vector
-                    matrix[:,i] = data.transpose() # append to input matrix
+                    vector = data.flatten() # reshape into vector (length: total number of voxels)
+                    matrix[:,i] = vector.transpose() # save column vector to matrix
                     i += 1 # update counter
                 else:
                     if (sub[0]=='9' and site=='NKI') or (sub[0]=='W' and site=='UW'):
                         print(f'Does not exist:\n\t{cope_path}\n')
+                
 
         if session == '1':
             matrix1 = np.array(matrix)
-            matrix_fn = f'/home/holland/Desktop/{roi}_S{session}_rest_thresh_zstat.csv'
+            matrix_fn = f'/Users/holland_brown_ra/Desktop/{roi}_S{session}_rest_zstat.csv'
             matrix1.tofile(matrix_fn, sep=',')
+
+            # mean_mat1 = np.array(sum_mat) * (1/num_subjects) # mean for this session
+            # meanmat_fn = f'/Users/holland_brown_ra/Desktop/{roi}_S{session}_rest_zstat_mean.csv'
+            # mean_mat1.tofile(meanmat_fn, sep=',')
+
         elif session == '2':
             matrix2 = np.array(matrix)
-            matrix_fn = f'/home/holland/Desktop/{roi}_S{session}_rest_thresh_zstat.csv'
+            matrix_fn = f'/Users/holland_brown_ra/Desktop/{roi}_S{session}_rest_thresh_zstat.csv'
             matrix2.tofile(matrix_fn, sep=',')
+
+            # mean_mat2 = np.array(sum_mat) * (1/num_subjects) # mean for this session
+            # meanmat_fn = f'/Users/holland_brown_ra/Desktop/{roi}_S{session}_rest_zstat_mean.csv'
+            # mean_mat2.tofile(meanmat_fn, sep=',')
 
 # %% Calculate difference between correlations S1 and S2 for each subject
 # NOTE: BandTogether = 0; WORDS! = 1
@@ -147,12 +172,28 @@ ix_tx0 = (np.array(TxLabels) == '0') # Boolean vector -> use as index
 delta_tx0 = matrix2[..., ix_tx0].mean(-1) - matrix1[..., ix_tx0].mean(-1) # time 2 - time 1 for BandTogether
 delta_tx1 = matrix2[..., ~ix_tx0].mean(-1) - matrix1[..., ~ix_tx0].mean(-1) # time 2 - time 1 for WORDS!
 delta_tx = delta_tx0 - delta_tx1 # difference between Tx groups
-plt.scatter(np.arange(100), delta_tx[delta_tx.argsort()][-100:][::-1]) # space x axis evenly; sort differences by magnitude
+
+colors = np.arange(100)  # gradient of colors based on x-axis index values
+plt.scatter(np.arange(100), delta_tx[delta_tx.argsort()][-100:][::-1], s=15, c=colors, cmap='viridis') # space x axis evenly; sort differences by magnitude
 # plt.plot(np.linspace(-6, 8, 100), np.linspace(-6, 8, 100))
+
+plt.xlabel('Index') # arbitrary index, just to make them equally spaced along x
+plt.ylabel(f'ROI-to-whole brain thresholded z-score change\n(BandTogether minus WORDS!)') # y shows the difference between Tx groups' time1-time2 differences
+
+roi_string = 'Left Middle Frontal Gyrus' # how you want ROI name to appear in the caption and title
+# caption = f'Here, higher values on the y-axis represent voxels where \npre-post-treatment change are most different\n between the BandTogether and WORDS!\n treatment groups in the {roi_string}.'
+# plt.text(50, -4, caption, ha='center')
+plt.title(f'{roi_string}-to-wholebrain functional activity change: BandTogether vs. WORDS!') # Title for the plot
+
+
 plt.show()
 
+# %% Get map that shows the same thing as above (differences between time 1 and time 2 between groups), but as a brain map
 
-# %% Load CSV connectivity matrices
+
+
+# %% Load CSV connectivity matrices: THIS DOESN'T WORK
+# NOTE: haven't been able to successfully load these CSV files; not sure why - too big?
 # chunk_size = 1000
 
 # matrix1 = pd.read_csv(f'/home/holland/Desktop/L_MFG_S1_rest_higherlev_copes.csv')
@@ -160,17 +201,89 @@ plt.show()
 # matrix2 = pd.read_csv(f'/home/holland/Desktop/L_MFG_S2_rest_higherlev_copes.csv')
 # print("Done reading session 2 CSV.")
 print(np.shape(matrix1))
-print(np.shape(matrix1))
+print(np.shape(matrix2))
 print(len(TxLabels))
 
 
 # %% Run model
-# model_result = create_mixed_effects_model(connectivity_data_session1, connectivity_data_session2, treatment_labels)
-# print(model_result.summary())
-
 # NOTE: session 2 is the dependent variable; we want to see if session 1 and Tx group have any effect on it
-# results = create_mixed_effects_model(matrix1,matrix2,TxLabels)
-# print(results.summary())
+TxLabels_int = np.array([int(val) for val in TxLabels]) # convert labels list to integer vector
+ix_tx0 = (np.array(TxLabels) == '0') # Boolean vector -> use as index
+band_time1 = matrix1[..., ix_tx0] # BandTogether mean session 1
+band_time2 = matrix2[..., ix_tx0] # BandTogether mean session 2
+words_time1 = matrix1[..., ~ix_tx0] # WORDS! mean session 1
+words_time2 = matrix2[..., ~ix_tx0] # WORDS! mean session 2
+
+# Combine the data
+df = pd.concat([
+    matrix_to_df(band_time1, 'BandTogether', 'time1'),
+    matrix_to_df(band_time2, 'BandTogether', 'time2'),
+    matrix_to_df(words_time1, 'WORDS!', 'time1'),
+    matrix_to_df(words_time2, 'WORDS!', 'time2')
+])
+
+# Reset the subject identifier to be unique across groups if necessary (helps account for subject variance)
+df['Subject'] = df['Group'] + '_S' + df['Subject'].astype(str)
+
+# Mixed Effects Model: Value ~ Time + Group + Time:Group
+model = smf.mixedlm("Value ~ Time * Group", df, groups=df["Subject"], re_formula="~Time")
+result = model.fit()
+
+print(result.summary())
+
+# %% Plot results of model
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# (1) see how well model fits the data ------------------------------------
+# Calculate fitted values from the model
+df['Fitted'] = result.fittedvalues
+
+# Plotting observed vs. fitted values
+plt.figure(figsize=(10, 6))
+sns.scatterplot(x=df['Value'], y=df['Fitted'], hue=df['Group'], style=df['Time'])
+plt.plot([df['Value'].min(), df['Value'].max()], [df['Value'].min(), df['Value'].max()], 'r--')  # Line of perfect fit
+plt.xlabel('Observed Values')
+plt.ylabel('Fitted Values')
+plt.title('Observed vs. Fitted Values')
+plt.show()
+
+# %% (2) Plot residuals to check the assumption of homoscedasticity and normality in residuals -----------------------
+# NOTE: homoscedasticity is the condition in which variance of the residuals is consistent
+# (2) Calculate residuals
+df['Residuals'] = result.resid
+
+plt.figure(figsize=(10, 6))
+sns.scatterplot(x=result.fittedvalues, y=result.resid, hue=df['Group'])
+plt.axhline(y=0, color='r', linestyle='--')
+plt.xlabel('Fitted Values')
+plt.ylabel('Residuals')
+plt.title('Residuals vs. Fitted Values')
+plt.show()
+
+# %% (3) Create plots of fixed effects (time and group) and effect of their interactions ----------------------------------
+# Using seaborn to create interaction plots
+plt.figure(figsize=(12, 8))
+sns.lineplot(data=df, x='Time', y='Value', hue='Group', style='Group', markers=True, dashes=False)
+plt.title('Interaction of Time and Group on Response')
+plt.show()
+
+# %% (4) Random effects due to subject variability ----------------------------------
+# Extract random effects
+re_frame = result.random_effects
+
+# Convert the random effects dictionary to DataFrame
+re_df = pd.DataFrame({k: v.values.flatten() for k, v in re_frame.items()}).T
+re_df.columns = ['Intercept', 'Slope']
+
+# Plot the random intercepts and slopes
+plt.figure(figsize=(12, 6))
+sns.histplot(re_df['Intercept'], kde=True, color='blue', label='Intercept')
+sns.histplot(re_df['Slope'], kde=True, color='green', label='Slope')
+plt.title('Distribution of Random Effects')
+plt.legend()
+plt.show()
+
 
 # %% TEST: Model
 # Convert inputs to numpy arrays if they are not already
