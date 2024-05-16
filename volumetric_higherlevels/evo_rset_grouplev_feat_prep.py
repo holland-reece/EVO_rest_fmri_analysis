19bb6e50760207e0af2c8f15c1af3a03aa220358
Holland Brown <hrreagor.brown@gmail.com>
2024-05-16 09:45:41 -0400

    add python script to run all Feat models

--- volumetric_higherlevels/evo_rset_grouplev_feat_prep.py
+++ volumetric_higherlevels/evo_rset_grouplev_feat_prep.py
@@ -2,15 +2,17 @@
 import glob
 from my_imaging_tools import fmri_tools
 
-datadir = '/Volumes/EVO_Estia/EVO_MRI/organized/UW'
+datadir = '/media/holland/EVO_Estia/EVO_MRI/organized/NKI'
 q = fmri_tools(datadir)
 
-# Make /reg dir in all lower-level Feat dirs (needed for Feat group level)
-cmd = [None]*2
-dirs = glob.glob('/Volumes/EVO_Estia/EVO_MRI/organized/UW/*/func/rest/rois/*/rest_lowerlev_vol/*.feat')
+# %% Make /reg dir in all lower-level Feat dirs (needed for Feat group level)
+cmd = [None]*1
+dirs = glob.glob('/media/holland/EVO_Estia/EVO_MRI/organized/NKI/*/func/rest/rois/*/rest_lowerlev_vol/*.feat')
 for dir in dirs:
-    cmd[0] = f'mkdir {dir}/reg' # create /reg dir
-    cmd[1] = f'cp {dir}/mean_func.nii.gz {dir}/reg' # move mean_func into new dir
+    # cmd[0] = f'mkdir {dir}/reg' # create /reg dir
+    # cmd[1] = f'cp {dir}/mean_func.nii.gz {dir}/reg' # move mean_func into new dir
+    cmd[0] = f'mv {dir}/reg/mean_func.nii.gz {dir}/reg/standard.nii.gz' # move mean_func into new dir
+
 
     q.exec_cmds(cmd)
 
@@ -20,6 +22,16 @@ cmd = [None]
 dirs = glob.glob('/Volumes/EVO_Estia/EVO_MRI/organized/NKI/*/func/rest/rois/*/rest_lowerlev_vol/*.feat/reg')
 for dir in dirs:
     cmd[0] = f'cp /Users/amd_ras/fsl/etc/flirtsch/ident.mat /{dir}/example_func2standard.mat'
+    q.exec_cmds(cmd)
+
+# %% Run group level Feat models
+rois = ['L_MFG','R_MFG','L_rACC','R_rACC','R_dACC']
+# datadir = '/media/holland/EVO_Estia/EVO_MRI/organized/NKI'
+# q = fmri_tools(datadir)
+
+cmd = [None]
+for roi in rois:
+    cmd[0] = f'feat /media/holland/EVO_Estia/EVO_rest_higherlev_vol/{roi}_grouplevel.fsf'
     q.exec_cmds(cmd)
 
 # %%
