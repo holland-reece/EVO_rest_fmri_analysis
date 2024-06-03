892d3f21f460e21337ae5b60ee989c81d1ac479d
Holland Reece Brown <86682821+holland-reece@users.noreply.github.com>
2024-06-03 15:25:16 -0400

    update comments

--- level1_withinsubjects_withinruns/create_task_onset_files.py
+++ level1_withinsubjects_withinruns/create_task_onset_files.py
@@ -12,17 +12,10 @@ import subprocess
 # import argparse
 from my_imaging_tools import fmri_tools
 
-# Get path to subject list text file from bash options 
-# parser = argparse.ArgumentParser(description='Resting-state volumetric lower-level analysis')
-# # parser.add_argument('evo_rest_lowlev_post_MEP_vol.py') # positional argument
-# parser.add_argument('-s', '--subjecttextlist') # option that takes a value
-# args = parser.parse_args()
-
 # Important dirs
 # home_dir = f'/media/holland/EVO_Estia/EVO_MRI/organized' # path to MNI brain template for FSL, fsf file, etc
 home_dir = f'/Volumes/EVO_Estia/EVO_MRI/organized' # path to MNI brain template for FSL, fsf file, etc
-
-# q = fmri_tools(studydir=datadir, subjectlist_text=args.subjecttextlist) # init functions and subject list
+onset_excel_path = f'' # dir containing excel files with onset times for each subject
 
 sessions = ['1','2'] # for Floop, there are 2 sessions (for most participants)
 runs = ['1','2'] # for Floop, there are 2 runs
@@ -38,4 +31,4 @@ for site in sites:
         for session in sessions:
             for run in runs:
                 onsetfile_path = f'{datadir}/{sub}/func/unprocessed/task/{task_name}/session_{session}/run_{run}'
-                command[0] = f'touch {onsetfile_path}/{task_name}_S{session}_R{run}_onset.txt'
+                command[0] = f'touch {onsetfile_path}/{task_name}_S{session}_R{run}_onset.txt' # create empty text file
